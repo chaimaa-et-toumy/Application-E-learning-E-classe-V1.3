1d676ad136b53975a1126e343b0c6e050a182867
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-03-15 18:56:38 +0100

    use model  form in the button create student

--- student.php
+++ student.php
@@ -2,6 +2,22 @@
 session_start();
 if(isset($_SESSION['user_Email'] )){
 ?>
+<!-- Form model -->
+<?php
+include 'page/connection.php';
+if(isset($_POST['create'])){
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Phone = $_POST['Phone'];
+    $Enroll_Number = $_POST['Enroll_Number'];
+    $Date_of_admission = $_POST['Date_of_admission'];
+    $q = "insert into student_list (Name,Email,Phone,Enroll_Number,Date_of_admission) values ( '" . $Name . "' , '" . $Email . "' ,  $Phone ,  $Enroll_Number  ,'" . $Date_of_admission . "' )";
+    $stmt = $conn -> prepare($q);
+    $stmt -> execute();
+    header('location: student.php');
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -42,8 +58,8 @@ if(isset($_SESSION['user_Email'] )){
                                     d="M12.6 11.375H1.39998C0.157481 11.375 -0.472519 12.8574 0.411231 13.7211L6.01123 19.2211C6.55811 19.7582 7.44623 19.7582 7.99311 19.2211L13.5931 13.7211C14.4681 12.8574 13.8425 11.375 12.6 11.375ZM6.99998 18.25L1.39998 12.75H12.6L6.99998 18.25ZM1.39998 8.625H12.6C13.8425 8.625 14.4725 7.14257 13.5887 6.2789L7.98873 0.7789C7.44186 0.241791 6.55373 0.241791 6.00686 0.7789L0.406856 6.2789C-0.468144 7.14257 0.157481 8.625 1.39998 8.625ZM6.99998 1.74999L12.6 7.24999H1.39998L6.99998 1.74999Z"
                                     fill="#00C1FE" />
                             </svg>
-                            <a href="page/creat_student.php" class="btn text-uppercase text-white button-student">Add new
-                                student</a>
+                            <button class="btn text-uppercase text-white button-student" data-bs-toggle="modal" data-bs-target="#myModal">Add new
+                                student</button>
                         </div>
 
                     </div>
@@ -51,6 +67,45 @@ if(isset($_SESSION['user_Email'] )){
 
                 </div>
             </div>
+            <!-- *************************Create student use model bs ********************-->
+            <div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                        <form class="p-4" method="POST" action="#">
+                            <div class="d-flex justify-content-center">
+                            <a href="student.php"><i class="fas fa-backward pe-5"></i></a>
+                            <p class="text-uppercase h4 text-center fw-bold"> Creat student </p>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Name</label>
+                                <input type="text" class="form-control" name="Name"  placeholder="Entre your name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="email" class="form-control" name="Email" placeholder="Entre your email"  required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Phone</label>
+                                <input type="number" class="form-control" name="Phone" placeholder="Entre your phone"  required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Enroll Number</label>
+                                <input type="number" class="form-control" name="Enroll_Number" placeholder="Entre your enroll number"  required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Date of admission</label>
+                                <input type="date" class="form-control" name="Date_of_admission"  placeholder="Entre Date of admission" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary w-100" name="create">create</button>
+                        </form>
+                        </div>
+                        </div>
+                    </div>
+            </div>
+
         </div>
     </main>
 
