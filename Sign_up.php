ed9b58e8e0fb4139ecdc5e8e07ab2679c7267a74
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-03-15 21:38:08 +0100

    save information to database by  using php

--- Sign_up.php
+++ Sign_up.php
@@ -1,6 +1,18 @@
 <?php
- include 'page/connection.php'; 
+ include 'page/connection.php';
+   
+    if(isset($_POST['SIGN_UP'])){
+        $Email = $_POST['Email'];
+        $password = $_POST['password'];
+        $full_name = $_POST['full_name'];
+        $password_check = $_POST['password_check'];
+        $q = "insert into comptes (Email , password , Full_name , password_check) values ( '" . $Email . "' , '" . $password . "' , '" . $full_name . "' , '" . $password_check . "') ";
+        $stmt = $conn -> prepare($q);
+        $stmt -> execute();
+        header('location: index.php');
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +31,7 @@
 <body class="bg-body">
     <main class="container-fluid">
         <div class="d-flex align-items-center justify-content-center vh-100">
-            <form method="GET" class="card form-class p-4">
+            <form method="POST" class="card form-class p-4">
                 <h1 class="fw-bold border-start border-5 ps-2 h2"
                     style="border-left-color: #00c1fe !important;">E-classe
                 </h1>
@@ -29,18 +41,12 @@
                     <p class="text-muted"> Entrer your credentials to access your account</p>
                 </div>
 
-                <div class="mb-2">
-                    <label>Full name</label>
-                    <input type="text" class="form-control mt-2 full_name" placeholder="Enter your Full name" name="full_name"> 
-                    <!-- <i class="fas fa-check-circle"></i> 
-                    <i class="fas fa-exclamation-circle"></i>
-                    <small>Invalid Name</small> -->
-                </div>
+               
 
 
                 <div class="mb-2">
                     <label>Email</label>
-                    <input type="email" class="form-control mt-2 Email" placeholder="Enter your email" name="Email">
+                    <input type="email" class="form-control mt-2 " placeholder="Enter your email" name="Email">
                     <!-- <i class="fas fa-check-circle"></i>  -->
                     <!-- <i class="fas fa-exclamation-circle"></i>  -->
                     <!-- <small>Invalid Email </small> -->
@@ -49,22 +55,31 @@
 
                 <div class="mb-2">
                     <label>Password</label>
-                    <input type="password" class="form-control mt-2 password" placeholder="Enter your password" name="password">
+                    <input type="password" class="form-control mt-2" placeholder="Enter your password" name="password">
                     <!-- <i class="fas fa-check-circle"></i>  -->
                     <!-- <i class="fas fa-exclamation-circle"></i>  -->
                      <!-- <small>Invalid password </small> -->
                 </div>
 
                 <div class="mb-2">
+                    <label>Full name</label>
+                    <input type="text" class="form-control mt-2" placeholder="Enter your full name" name="full_name"> 
+                    <!-- <i class="fas fa-check-circle"></i> 
+                    <i class="fas fa-exclamation-circle"></i>
+                    <small>Invalid Name</small> -->
+                </div> 
+
+                <div class="mb-2">
                     <label> Password Check</label>
-                    <input type="password" class="form-control mt-2 password_check" placeholder="check your password" name="password">
+                    <input type="password" class="form-control mt-2" placeholder="check your password" name="password_check">
                     <!-- <i class="fas fa-check-circle"></i> -->
                     <!-- <i class="fas fa-exclamation-circle"></i>  -->
                      <!-- <small>Invalid password </small> -->
                 </div>
 
+               
                 <div class="mt-2">
-                    <input type="submit" value="SIGN UP"  name="SIGN-UP" class="btn bg-info w-100 text-white sign_up">
+                    <input type="submit" value="SIGN UP"  name="SIGN_UP" class="btn bg-info w-100 text-white">
                 </div>
             </form>
         </div>
