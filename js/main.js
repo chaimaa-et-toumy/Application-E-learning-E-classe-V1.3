14bdade2e6a35af6e2da0a1e888757971a7ccada
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-03-17 13:45:43 +0100

    validation js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+let   form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
 //////////  side bar  //////////
 function togglesidebar() {
     var sidebar = document.querySelector(".myside-bar");
@@ -10,76 +10,73 @@ function togglesidebar() {
 }
 
 //////////// sign up /////////////
-document.getElementById("sign_up").addEventListener('click', e => {
-    // e.preventDefault();
+document.getElementById("sign_up").addEventListener('click', () => {
     validateInputs();
 });
 
-function setError (element, message) {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
+function invalid (element, message) {
+    let inputControl = element.parentElement;
+    let errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
     inputControl.classList.remove('success')
 }
 
-function setSuccess (element){
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
+function success (element){
+    let inputControl = element.parentElement;
+    let errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
 
-const exp =/^[A-Za-z]{4,}$/;
-const expEmail =/[a-z][0-9]*@[a-z]+\.[a-z]{2,3}/;
+let exp =/^[A-Za-z]{4,}$/;
+let expEmail =/[a-z][0-9]*@[a-z]+\.[a-z]{2,3}/;
 
 function validateInputs (){
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+    let usernameValue = username.value.trim();
+    let emailValue = email.value.trim();
+    let passwordValue = password.value.trim();
+    let password2Value = password2.value.trim();
     var i = 0;
 
     if(usernameValue === '') {
-        setError(username, 'Username is required');
+        invalid(username, 'Username is required');
     } 
 	else if (exp.test(usernameValue) == false){
-		setError(username, 'Provide a valid username ');
+		invalid(username, 'Provide a valid username ');
 	} 
 	else {
-        setSuccess(username);
+        success(username);
         i++; 
     }
 
     if(emailValue === '') {
-        setError(email, 'Email is required');
+        invalid(email, 'Email is required');
     } 
 	else if (expEmail.test(emailValue) == false) {
-		    setError(email, 'Provide a valid email address');
+		    invalid(email, 'Provide a valid email address');
 	}
 	else {
-        setSuccess(email);
+        success(email);
         i++; 
     }
 
     if(passwordValue === '') {
-        setError(password, 'Password is required');
+        invalid(password, 'Password is required');
     } else if (passwordValue.length < 6 ) {
-        setError(password, 'Password must be at least 6 character.')
+        invalid(password, 'Password must be at least 6 character.')
     } else {
-        setSuccess(password);
+        success(password);
         i++; 
     }
 
     if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
+        invalid(password2, 'Please confirm your password');
     } else if (password2Value !== passwordValue) {
-        setError(password2, "enter same password");
+        invalid(password2, "enter same password");
     } else {
-        setSuccess(password2);
+        success(password2);
         i++; 
     }
 
